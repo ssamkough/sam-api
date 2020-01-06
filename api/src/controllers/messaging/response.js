63fb164f5427ded4e841df603ce93a1c3ae8843f
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-01-06 14:24:34 -0500

    Added initial keyword functionality

--- api/src/controllers/messaging/response.js
+++ api/src/controllers/messaging/response.js
@@ -1,9 +1,16 @@
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const response = async (req, res, next) => {
+    const message = req.body.Body.toLowerCase();
     const twiml = new MessagingResponse();
+    const keywords = ['note'];
+    
+    if (message.includes('note', 0)) {
+        twiml.message('Oi!');
+    } else {
+        twiml.message('Twilio here!');
+    }
 
-    twiml.message('This is a Twilio Response!');
     
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
