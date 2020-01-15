c9b82319761a025abaf9ad6028192f45106b5204
Sammy Samkough <sammysamkough@gmail.com>
2020-01-14 23:47:29 -0500

    Refactored into async/await

--- src/middleware/messageHandler.js
+++ src/middleware/messageHandler.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
-const messageHandler = (error, req, res, next) => {
-    client.messages
-    .create({
-       body: 'This is a twilio message.',
-       from: `+${process.env.TWILIO_FROM_NUMBER}`,
-       to: `+${process.env.TWILIO_TO_NUMBER}`
-     })
-    .then(message => console.log(message.sid));
-}
+const messageHandler = async (error, req, res, next) => {
+  const message = await client.messages.create({
+    body: "This is a twilio message.",
+    from: `+${process.env.TWILIO_FROM_NUMBER}`,
+    to: `+${process.env.TWILIO_TO_NUMBER}`
+  });
+
+  console.log(message.sid);
+};
 
 export default messageHandler;
