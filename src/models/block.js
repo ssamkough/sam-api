6e5fe30d72f28266798636810fdb313fa761bb03
Sammy Samkough <sammysamkough@gmail.com>
2020-01-14 23:47:54 -0500

    Refactored to match mongoose schema code

--- src/models/block.js
+++ src/models/block.js
@@ -1,7 +1,15 @@
-export default (sequelize, DataTypes) => {
-  const Block = sequelize.define("Block", {
-    name: DataTypes.STRING
-  });
-  Block.associate = function(models) {};
-  return Block;
-};
+import { Schema, model } from "mongoose";
+
+const BlockSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+});
+
+export default model("Block", BlockSchema);
