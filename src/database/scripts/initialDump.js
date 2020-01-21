064e6777d4752af24578e9a13de803fd0871d9b5
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-01-21 16:57:12 -0500

    Updated import script

--- src/database/scripts/initialDump.js
+++ src/database/scripts/initialDump.js
@@ -1,46 +1,42 @@
 import Papa from "papaparse";
 import fs from "fs";
-import path from "path";
 
-import User from "../../models/User";
 import Block from "../../models/Block";
+import User from "../../models/User";
 
-export default async (req, res, next) => {
-  console.log("test");
-  const pathString = `${__dirname}/../../models`;
-  const totalFiles = fs.readdirSync(pathString).length;
-
-  fs.readdirSync(pathString, (err, files) => {
-    files.forEach((file, index) => {
-      console.log(index + file);
-      Papa.parse(file, {
-        header: true,
-        step: function(results) {
-          console.log("Row:", results.data);
-        },
-        complete: function(results) {
-          console.log("Finished:", results.data);
+export default async => {
+  const pathString = `${__dirname}/../../assets/models`;
+  const files = fs.readdirSync(pathString);
+  
+  files.forEach((file, index) => {
+    console.log("Importing " + file + "...");
+    const url = pathString + "/" + file;
+    const rsFile = fs.createReadStream(url);
+    Papa.parse(rsFile, {
+      header: true,
+      step: (results) => {
+        if (file == 'blocks.csv') {
+           const block = await new Block({
+              name: results.data.name
+            });
+            await block.save();
+        } else if (file == 'users.csv') {
+            const user = await new User({
+              first_name: results.data.first_name,
+              last_name: results.data.last_name,
+              email: results.data.email,
+              birth_date: results.data.birth_date,
+              about_me: results.data.about_me,
+              profile_image_url: results.data.profile_image_url,
+              job_title: results.data.job_title,
+              company: results.data.company
+            });
+            await user.save();
         }
-      });
+      },
+      complete: (results) => {
+        console.log("Finished importing " + file + "!")
+      }
     });
   });
-
-  //  const block = await new Block({
-  //     name: req.body.name
-  //   });
-
-  //   await block.save();
-
-  //   const user = await new User({
-  //     first_name: req.body.first_name,
-  //     last_name: req.body.last_name,
-  //     email: req.body.email,
-  //     birth_date: req.body.birth_date,
-  //     about_me: req.body.about_me,
-  //     profile_image_url: req.body.profile_image_url,
-  //     job_title: req.body.job_title,
-  //     company: req.body.company
-  //   });
-
-  //   await user.save();
 };
