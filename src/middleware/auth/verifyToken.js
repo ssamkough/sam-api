ba04a4095433089918ad854b9c2466bff74c6115
Sammy Samkough <sammysamkough@gmail.com>
2020-02-13 00:18:39 -0500

    Refactored code

--- src/middleware/auth/verifyToken.js
+++ src/middleware/auth/verifyToken.js
@@ -1,14 +1,17 @@
 import jwt from "jsonwebtoken";
 
-export default verifyToken = (req, res, next) => {
-  const token = req.header("auth-token");
+export default (req, res, next) => {
+  const token = req.header("sam-api-token");
   if (!token) {
+    console.error("Headers: " + JSON.stringify(req.headers));
+    console.error("Token: " + token);
     return res.status(401).send("Access Denied!");
   }
 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
+    next();
   } catch (err) {
     res.status(400).send("Invalid Token!");
   }
