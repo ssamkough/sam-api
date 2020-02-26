4d8ca62b168f5e7c5cb6fddc0fffa1df0794eea3
Sammy Samkough <sammysamkough@gmail.com>
2020-02-25 22:28:03 -0500

    Added cors so it gets used globally

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 import express from "express";
 import logger from "morgan";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import router from "./router";
 import errorHandler from "./middleware/errorHandler";
@@ -11,6 +12,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/api", router);
 
 app.get("/", (req, res) => {
